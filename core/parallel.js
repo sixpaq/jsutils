5e2d39aa76118760a5d9e7631a660608fa6de602
Mike Wijnen <sixpaq@gmail.com>
2018-02-12 11:28:35 +0100

    feat: added limit to parallel task processing

--- core/parallel.js
+++ core/parallel.js
@@ -1,3 +1,5 @@
+const { loopWhile } = require('deasync');
+
 /**
  * Performs a series of tasks in parallel. When all tasks are
  * finished, the callback function is called.
@@ -6,20 +8,35 @@
  * @param {any} callback (err, results)
  * @returns 
  */
-async function parallel(tasks, callback) {
+async function parallel(tasks, max = -1, callback) {
+  if (callback === undefined && typeof max === 'function') {
+    callback = max;
+    max = -1;
+  }
+
   const errors = [];
   const results = [];
   const proms = [];
+  let limit = 0;
+
   for (let i = 0; i < tasks.length; i += 1) {
     let task = tasks[i];
 
     const prom = new Promise((resolve, reject) => {
+      limit += 1;
+      if (max > 0) {
+        loopWhile(() => {
+          return limit > max;
+        });
+      }
       task((err, result) => {
         if (err) {
           errors.push(err);
+          limit -= 1;
           return resolve({ error: err });
         }
         results.push(result);
+        limit -= 1;
         return resolve({ error: null, result: result });
       });
     });
